de24d9942e709c0216f6db04800a03fae3c796e0
Moon-of-jupiter <ansgar.brunskog@gmail.com>
2026-01-16 18:08:08 +0100

    init

---
+++ cubemodel.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "src/model.h"
+class CubeModel : public Model
+{
+    
+
+};
+
--- shaders/pixel_shader.hlsl
+++ shaders/pixel_shader.hlsl
@@ -16,7 +16,11 @@ float4 PS_main(PSIn input) : SV_Target
 {
 	// Debug shading #1: map and return normal as a color, i.e. from [-1,1]->[0,1] per component
 	// The 4:th component is opacity and should be = 1
-	return float4(input.Normal*0.5+0.5, 1);
+	//return float4(input.Normal*0.5+0.5, 1);
+	
+    float lambert = (dot(input.Normal, normalize(float3(0.5, 0.5, 1))) + 1) / 2;
+	
+    return float4(lambert, lambert, lambert, 1);
 	
 	// Debug shading #2: map and return texture coordinates as a color (blue = 0)
 //	return float4(input.TexCoord, 0, 1);
